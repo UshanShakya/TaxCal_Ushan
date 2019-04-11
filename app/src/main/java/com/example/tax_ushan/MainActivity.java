060d3d670c8be4e4560fe08cffad7e960ebb987a
UshanShakya <usnshakya4@gmail.com>
2019-04-11 23:33:31 +0545

    asdf

--- app/src/main/java/com/example/tax_ushan/MainActivity.java
+++ app/src/main/java/com/example/tax_ushan/MainActivity.java
@@ -7,10 +7,12 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText etIncome, etTax;
+    EditText etIncome;
+    TextView tvTax;
     Button btnCalcualte;
 
 
@@ -20,29 +22,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         etIncome= findViewById(R.id.etIncome);
-        etTax= findViewById(R.id.etTax);
+        tvTax= findViewById(R.id.tvTax);
 
         btnCalcualte= findViewById(R.id.btnCalculate);
 
         btnCalcualte.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int income, tax;
+                double income, tax;
 
                 income = Integer.parseInt(etIncome.getText().toString()) *12;
 
-                if (income>1 && income<200000){
-                    tax = income * 15/100;
-                    etTax.setText(Integer.toString(tax));
-                }
-                else if (income>200000 && income <350000) {
-                    tax = (200000 * 1/100) +(income-200000)*15/100;
-                    etTax.setText(Integer.toString(tax));
-                }
-                else if (income>350000) {
-                    tax = 200000 * 1/100 +150000*15/100+ (income - 350000)*25/100;
-                    etTax.setText(Integer.toString(tax));
-                }
+                TaxCalculator taxCalculator= new TaxCalculator(income);
+                tax= taxCalculator.Tax();
+                tvTax.setText(Double.toString(tax));
 
                 InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
                 imm.hideSoftInputFromWindow(btnCalcualte.getWindowToken(), InputMethodManager.RESULT_UNCHANGED_SHOWN);
